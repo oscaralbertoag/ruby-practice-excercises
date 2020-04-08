d1079c9e5c6e7fa26f979ffc34c793de0dcb8af3
oscaralbertoag <oaahh4@mst.edu>
2020-04-07 21:15:32 -0600

    Clean up corrupt-grades solution

--- corrupt-grades/corrupt_grades.rb
+++ corrupt-grades/corrupt_grades.rb
@@ -1,11 +1,5 @@
 def corrupt_grades(grades, paid)
-    copy_of_grades = grades
-    copy_of_grades.append(paid ? 10 : 5)
-    sum = 0
-    
-    grades.each do |grade|
-        sum += grade
-    end
-
-    sum.to_f/copy_of_grades.size
+    altered_grades = grades
+    altered_grades.append(paid ? 10 : 5)
+    altered_grades.sum.to_f/altered_grades.size
 end
