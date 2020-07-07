131b20c13989f473d114201a5a9d57e80362e459
Erika Alvarez <8erika7@gmail.com>
2020-07-07 16:59:36 +0200

    Solution for Odd-Up-Even-Down

--- odd-up-even-down/lib/odd_up_even_down_solution.rb
+++ odd-up-even-down/lib/odd_up_even_down_solution.rb
@@ -1,3 +1,11 @@
 def transform(numbers)
-  # Your code here
+  transformed = []
+  numbers.each do |n|
+    if n.even?
+      transformed << (n - 1)
+    elsif n.odd?
+      transformed << (n + 1)
+    end
+  end
+  transformed
 end
