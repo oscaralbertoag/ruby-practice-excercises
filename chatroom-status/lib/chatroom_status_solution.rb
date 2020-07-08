164b755a9f802040d2c98d3244016b4f00b30ced
Erika Alvarez <8erika7@gmail.com>
2020-07-08 17:40:42 +0200

    Elimination of variable n

--- chatroom-status/lib/chatroom_status_solution.rb
+++ chatroom-status/lib/chatroom_status_solution.rb
@@ -1,11 +1,10 @@
 def chatroom_status(users)
   return 'no one online' unless users != []
-  n = 0
     if users.size == 1
       return "#{users[0]} online"
     elsif users.size == 2
       return "#{users[0]} and #{users[1]} online"
     elsif users.size > 2
-      return "#{users[0]}, #{users[1]} and #{n = users.size - 2} more online"
+      return "#{users[0]}, #{users[1]} and #{users.size - 2} more online"
     end
 end
