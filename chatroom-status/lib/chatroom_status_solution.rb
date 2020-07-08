150f758e42af00d1cd109adc0199ca97ba38919c
Erika Alvarez <8erika7@gmail.com>
2020-07-08 17:38:07 +0200

    Chatroom-status Solution

--- chatroom-status/lib/chatroom_status_solution.rb
+++ chatroom-status/lib/chatroom_status_solution.rb
@@ -1,3 +1,11 @@
 def chatroom_status(users)
-  # Your code here
+  return 'no one online' unless users != []
+  n = 0
+    if users.size == 1
+      return "#{users[0]} online"
+    elsif users.size == 2
+      return "#{users[0]} and #{users[1]} online"
+    elsif users.size > 2
+      return "#{users[0]}, #{users[1]} and #{n = users.size - 2} more online"
+    end
 end
