4999b3f799c714ee76624c1f6f4d1cd15310a502
Erika Alvarez <8erika7@gmail.com>
2020-07-09 15:26:40 +0200

    Solution to Odd Up, Even Down using map

--- odd-up-even-down/lib/odd_up_even_down_solution.rb
+++ odd-up-even-down/lib/odd_up_even_down_solution.rb
@@ -1,11 +1,3 @@
 def transform(numbers)
-  transformed = []
-  numbers.each do |n|
-    if n.even?
-      transformed << (n - 1)
-    elsif n.odd?
-      transformed << (n + 1)
-    end
-  end
-  transformed
+  numbers.map { |n| n.odd? ? n + 1 : n - 1 }
 end
