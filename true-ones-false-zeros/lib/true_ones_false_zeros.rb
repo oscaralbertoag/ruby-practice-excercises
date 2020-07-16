0915d125ea12fe97d718190168520ab36b148cc5
Erika Alvarez <8erika7@gmail.com>
2020-07-16 17:53:11 +0200

    Solucion Erika a True Ones, False zeros

--- true-ones-false-zeros/lib/true_ones_false_zeros.rb
+++ true-ones-false-zeros/lib/true_ones_false_zeros.rb
@@ -1,3 +1,11 @@
 def integer_boolean(input)
-  # Your code here
+  new_input = []
+  input.each_char do |char|
+    if char == '0'
+      new_input << false
+    else
+      new_input << true
+    end
+  end
+  new_input
 end
