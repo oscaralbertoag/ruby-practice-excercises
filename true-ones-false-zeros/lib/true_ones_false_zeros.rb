5f81a069b4e7e3d7ccceae5ca8266621078d5c71
Erika Alvarez <8erika7@gmail.com>
2020-07-22 15:00:04 +0200

    Changed to ternary operator for it to be cleaner

--- true-ones-false-zeros/lib/true_ones_false_zeros.rb
+++ true-ones-false-zeros/lib/true_ones_false_zeros.rb
@@ -1,11 +1,7 @@
 def integer_boolean(input)
   new_input = []
   input.each_char do |char|
-    if char == '0'
-      new_input << false
-    else
-      new_input << true
-    end
+    char != '0' ? new_input << true : new_input << false
   end
   new_input
 end
